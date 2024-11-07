234afac60d682a4f7ad764642af55a094900fc89
Harianto van Insulinde <hariantoatwork@gmail.com>
2024-11-07 15:08:53 +0100

    PrimeVue and Tailwind config

--- primevue-nuxt-3/app.vue
+++ primevue-nuxt-3/app.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="container mx-auto p-4">
-    <DomainTable />
+  <div class="landing-page">
+    <header class="header">
+      <h1>Welcome to Our Landing Page</h1>
+      <p>Explore the features of our application.</p>
+    </header>
+    <main class="content">
+      <Button label="Get Started" icon="pi pi-check" class="p-button-success" />
+      <Button
+        label="Learn More"
+        icon="pi pi-info-circle"
+        class="p-button-info"
+      />
+    </main>
+    <footer class="footer">
+      <p>&copy; 2023 Your Company</p>
+    </footer>
   </div>
 </template>
 
+<script setup>
+import { Button } from 'primevue/button'
+</script>
+
 <style>
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
+.landing-page {
+  text-align: center;
+  padding: 2rem;
+}
+
+.header {
+  margin-bottom: 2rem;
+}
+
+.content {
+  margin-bottom: 2rem;
+}
+
+.footer {
+  margin-top: 2rem;
+  font-size: 0.8rem;
+  color: #888;
 }
 </style>
--- primevue-nuxt-3/nuxt.config.js
+++ primevue-nuxt-3/nuxt.config.js
@@ -1,9 +1,7 @@
-import Aura from '@primevue/themes/aura';
+import Aura from '@primevue/themes/aura'
 
 export default defineNuxtConfig({
-  modules: [
-    '@primevue/nuxt-module'
-  ],
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
 
   primevue: {
     autoImport: true,
@@ -18,17 +16,25 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap' }
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: true
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap'
+        }
       ]
     }
   },
 
   css: [
     'primeicons/primeicons.css',
-    '~/assets/scss/index.scss'
+    '~/assets/scss/index.scss',
+    '~/assets/scss/tailwind.scss'
   ],
-  
+
   devtools: { enabled: false },
   compatibilityDate: '2024-10-29'
 })
